508ab567c137c353e480ed10061218297917aa98
K <kitkatapioca@gmail.com>
2025-07-07 17:44:42 -0400

    tabla en archivo MostrarProyectos
    se creó la tabla basica para mostrar lo s proyectos insertados

--- src/pages/Mostrarproyectos.tsx
+++ src/pages/Mostrarproyectos.tsx
@@ -2,7 +2,23 @@ import React from 'react'
 import { Proyecto } from '@/app/interfaces/IProyecto'
 
 export default function Mostrarpersonas() {
+  
   return (
-    <div>Mostrarproyectos</div>
+    <>
+      <h2>Proyectos</h2>
+      <table>
+        <thead>
+          <th>Nombre</th>
+          <th>Presupuesto</th>
+          <th>Fecha</th>
+          <th>Tipo de proyecto</th> 
+          <th>Descripción</th>
+          <th>Acción</th>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    </>
   )
 }
