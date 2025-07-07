1ce86fd39c0442526c21359b80a90b9bdb7c1fed
K <kitkatapioca@gmail.com>
2025-07-07 17:04:39 -0400

    habdleRegistrsar + handle Proyecto
    se implementó la funcion handleRegistrar y handle proyecto para la inserción de los datos en el local storage además de el atributo onclick y onchange para los elementos del formulario con una validación inicial para el campo de presupuesto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,34 +15,61 @@ export default function Home() {
   const [proyectos, setproyectos] = useState<Proyecto[]>([])
   const [proyecto, setproyecto] = useState(initialStateProyecto)
     
-    const handleRegistrar = ()=>{
-      miStorage.setItem("proyectos", JSON.stringify([...proyectos, proyecto]))
+  const handleRegistrar = ()=>{
+    miStorage.setItem("proyectos", JSON.stringify([...proyectos, proyecto])) 
     }
 
-
+  const handleProyecto = (name:string, value:string)=>{setproyecto(
+    {...proyecto, [name]:value})
+    if(name == "presupuesto" && Number(value) <= 0){
+      console.log("el presupuesto debe ser mayor a 0")
+    }
+  
+    }
 
   return (
     <>
-    <h1>Bienvenido al sistema de gestión</h1>
-      <form>
-        <h3>Registrar proyecto</h3>
-        <label>Nombre: </label>
-        <input type="text" name="nombre" placeholder="Nombre del proyecto"/><br/>
-        <label>Presupuesto: </label>
-        <input type="number" name="presupuesto" placeholder="100000"/><br/>
-        <label>Fecha: </label>
-        <input type="date" name="fecha" /><br/>
-        <label>Tipo: </label>
-        <select name="opcionesTipo" id="">
-          <option value="">Seleccione</option>
-          <option value="">Comunidad</option>
-          <option value="">Educación</option>
-          <option value="">Salud</option>
-        </select><br/>
-        <label htmlFor="">Descripción</label><br/>
-        <textarea name="descripcion" id=""></textarea><br/>
-        <button type="submit">Registrar</button>
-      </form>
+      <h1>Bienvenido al sistema de gestión</h1>
+        <form>
+          <h3>Registrar proyecto</h3>
+          <label>Nombre: </label>
+          <input 
+          type="text" name="nombre"    placeholder="Nombre del proyecto"
+          onChange={(e)=>{handleProyecto(e.currentTarget.name, e.currentTarget.value)}}/><br/>
+
+          <label>Presupuesto: </label>
+          <input 
+          type="number" 
+          name="presupuesto" 
+          placeholder="100000"
+          onChange={(e)=>{handleProyecto(e.currentTarget.name, e.currentTarget.value)}}/><br/>
+
+          <label>Fecha: </label>
+          <input 
+          type="date" 
+          name="fecha" 
+          onChange={(e)=>{handleProyecto(e.currentTarget.name, e.currentTarget.value)}}/><br/>
+
+          <label>Tipo: </label>
+          <select 
+          name="opcionesTipo" 
+          id=""
+          onChange={(e)=>{handleProyecto(e.currentTarget.name, e.currentTarget.value)}}>
+            <option value="">Seleccione</option>
+            <option value="">Comunidad</option>
+            <option value="">Educación</option>
+            <option value="">Salud</option>
+          </select><br/>
+
+          <label htmlFor="">Descripción</label><br/>
+          <textarea 
+          name="descripcion" 
+          id=""
+          onChange={(e)=>{handleProyecto(e.currentTarget.name, e.currentTarget.value)}}></textarea><br/>
+
+          <button
+          onClick={()=>handleRegistrar()}>Registrar</button>
+        </form>
     </>
   );
 }
